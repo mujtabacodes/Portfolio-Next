8190eceb7510ad24532e74e05351d89b80dfb6ff
Mujtaba Shafique <mujtaba.shafique@outlook.com>
2024-05-29 18:38:09 +0500

    Add Footer and Testimonail in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,14 +7,18 @@ import Nav from "@/components/nav/nav";
 import { Header } from "@/components/header/header";
 import Projects from "@/components/projects";
 import Experience from "@/components/experiences";
+import Testimonails from "@/components/testimonials";
+import { Footer } from "@/components/footer";
 export default function Home() {
   return (<React.Fragment>
      {/* <div className="w-full h-screen  bg-slate-500 bg-no-repeat bg-center text-orange dark:bg-black relative"> */}
     <div> 
      <Nav/>
      <Header/>
-        <Projects/>
-        <Experience/>
+      <Projects/>
+      <Experience/>
+      <Testimonails/>
+      <Footer/>
         </div> 
     </React.Fragment>
   );
