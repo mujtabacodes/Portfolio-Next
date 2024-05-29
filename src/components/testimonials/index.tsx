a281d16466a625d1d463f20374b669ea90068910
Mujtaba Shafique <mujtaba.shafique@outlook.com>
2024-05-29 18:32:44 +0500

    Added testimonail component

---
+++ src/components/testimonials/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { SectionHeader } from '../section-header'
+
+const Testimonails = () => {
+  return (
+    <div>
+        <SectionHeader heading='Testimonails' subTitle='Something which matters' align='left'/>
+    </div>
+  )
+}
+
+export default Testimonails
